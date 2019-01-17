ddc2160d9b373d55c5cc5de57f3d99eefe6b67e8
3A532053 <showhk61@gmail.com>
2019-01-17 22:11:29 +0800

    新增TaskController destroy方法與路由

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,5 @@ Route::get('/home', function () {
 Route::get('/tasks', 'TaskController@index');
 Route::post('/task', 'TaskController@store');
 Route::delete('/task/{task}', 'TaskController@destroy');
+
+Route::delete('/task/{task}', 'TaskController@destroy');
