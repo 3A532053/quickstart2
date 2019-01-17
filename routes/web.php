6d51e73d827afa8c25f46944e89fb3d27caddcab
3A532053 <showhk61@gmail.com>
2019-01-17 21:18:49 +0800

    新增TaskController及相關路由

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::auth();
 Route::get('/home', function () {
     return view('home.home');
 });
+
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
